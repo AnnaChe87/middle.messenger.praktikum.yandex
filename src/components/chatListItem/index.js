2af6d9c5ab675f92b0480da7eaf165bd1c6ca54c
Черных Анна <4uto4ka@gmail.com>
2023-01-08 15:50:28 +0400

    minor fixes

--- src/components/chatListItem/index.js
+++ src/components/chatListItem/index.js
@@ -1,6 +1,10 @@
+import { getTime } from "../../utils/dateTime";
 import template from "./chatListItem.hbs";
 import "./chatListItem.scss";
 
 export default function(props = {}) {
-    return template(props);
+    return template({
+        ...props,
+        formattedTime: getTime(props.last_message.time)
+    });
 }
