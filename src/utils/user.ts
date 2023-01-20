6070039c3233311859339302847870b5ff23a15e
Черных Анна <chernykhav@it2g.ru>
2023-01-20 14:01:34 +0400

    import fix

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,9 +1,8 @@
-import * as currentUserData from "../mock/currentUser.json";
-import { UserModel } from "../mock/mock.types";
+import { data, UserModel } from "../mock";
 
 export function getUserName(user: UserModel) {
   const { first_name, second_name, login } = user;
-  if (login === currentUserData.login) {
+  if (login === data.currentUser.login) {
     return "Вы";
   }
   return `${first_name} ${second_name}`;
